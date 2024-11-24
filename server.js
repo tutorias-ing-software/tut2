50d22cdc79eaddf35ee1899d48f87952e0f89378
luis <yo98111@gmail.com>
2024-11-24 19:12:10 -0300

    arreglo del server.js
    arreglo

--- server.js
+++ server.js
@@ -3,42 +3,7 @@ const express = require('express');
 const socketIo = require('socket.io');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-<<<<<<< HEAD
-const authRoutes = require('./routes/authRoutes'); // Rutas de autenticación y usuarios
-const db = require('./config/db'); // Configuración de conexión a la base de datos
-const cookieParser = require('cookie-parser');
-require('dotenv').config(); // Variables de entorno
-
-const app = express(); // Inicializa `app`
-
-const PORT = process.env.PORT || 3000;
-const secretKey = process.env.JWT_SECRET; // Llave secreta para JWT
-
-// Middleware
-app.use(cookieParser()); // Manejo de cookies
-app.use(cors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:3000', // Configuración de CORS
-    credentials: true, // Permite el uso de credenciales como cookies
-}));
-app.use(express.json()); // Para manejar JSON en solicitudes
-app.use(bodyParser.urlencoded({ extended: true })); // Para manejar formularios URL-encoded
-
-// Servir archivos estáticos
-app.use(express.static('public'));
-app.use(express.static('images'));
-
-// Ruta principal para servir la página inicial
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html'); // Asegúrate de que el archivo `index.html` esté en la carpeta `public`
-});
-
-// Usar las rutas de autenticación
-app.use('/api', authRoutes); // Mapeo de rutas API
-
-// Iniciar el servidor
-app.listen(PORT, () => {
-    console.log(`Servidor escuchando en http://localhost:${PORT}`);
-=======
+const db = require('./config/db');
 const authRoutes = require('./routes/authRoutes'); // Rutas de autenticación
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
@@ -54,14 +19,18 @@ const io = socketIo(server, {
 
 const PORT = process.env.PORT || 3000;
 const users = {};
+const secretKey = process.env.JWT_SECRET;
 
 // Middleware
 app.use(cookieParser());
 app.use(cors({
     origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+    credentials: true,
 }));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
 app.use(express.static('public'));
 app.use(express.static('images'));
 
@@ -101,5 +70,4 @@ io.on('connection', (socket) => {
 // Iniciar el servidor
 server.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
->>>>>>> b145dee64ad9abb45af4e6027a13c1c99641f33a
 });
